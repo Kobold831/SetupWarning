9034d43cf3de84ba23f8a032bc49826382995b59
Kobold831 <k1z3y1.y15n5d1.1022@gmail.com>
2023-10-15 21:43:46 +0900

    Initial commit

---
+++ app/src/main/java/com/saradabar/setupwarning/MainActivity.java
@@ -0,0 +1,26 @@
+package com.saradabar.setupwarning;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+        getWindow().getDecorView().setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                        | View.SYSTEM_UI_FLAG_FULLSCREEN);
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        finishAndRemoveTask();
+    }
+}
