be08f9f1363546fa55bb65156f6519df9a4ffe82
Syuugo <pub@s1204.me>
2023-10-18 17:21:36 +0900

    Fixes

--- app/src/main/java/com/saradabar/setupwarning/MainActivity.java
+++ app/src/main/java/com/saradabar/setupwarning/MainActivity.java
@@ -2,7 +2,8 @@ package com.saradabar.setupwarning;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.view.View;
+
+import static android.view.View.*;
 
 public class MainActivity extends Activity {
 
@@ -11,11 +12,16 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
         getWindow().getDecorView().setSystemUiVisibility(
-                View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
-                        | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
-                        | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
-                        | View.SYSTEM_UI_FLAG_FULLSCREEN);
+            SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+                + SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                + SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                + SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                + SYSTEM_UI_FLAG_FULLSCREEN
+        );
+    }
+
+    @Override
+    public void onBackPressed() {
     }
 
     @Override
